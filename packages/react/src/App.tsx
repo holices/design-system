af9e874835d3bf71258cf04e27d9a545241044c3
Holices Junior <dev.holices@gmail.com>
2024-06-30 11:06:29 +0100

    feat: added ReactJS

---
+++ packages/react/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/Button/styles'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Button>Hello, world!</Button>
+    </div>
+  )
+}
+
+export default App
